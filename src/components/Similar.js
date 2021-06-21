95b023dd74cc4a4160535833ac42a23b88de9a8a
shamar-morrison <shamar.webdev@gmail.com>
2021-06-21 15:20:36 -0500

    Add browser reload functionality without losing movie data

--- src/components/Similar.js
+++ src/components/Similar.js
@@ -1,5 +1,6 @@
 import { BASE_IMG_URL } from '../logic/requests';
 import noImageFound from '../img/no-img-found.png';
+import { Link } from 'react-router-dom';
 
 const Similar = ({ similarMovies, onClick }) => {
 	const scrollToTop = () => {
@@ -13,20 +14,22 @@ const Similar = ({ similarMovies, onClick }) => {
 				<ul className="similar-movies--list">
 					{similarMovies.slice(0, 6).map((movie, i) => {
 						return (
-							<li
-								className="similar-movies--item"
-								onClick={() => {
-									onClick(movie);
-									scrollToTop();
-								}}
-								key={i}
-							>
-								<img
-									loading="lazy"
-									src={movie.poster_path ? `${BASE_IMG_URL}${movie.poster_path}` : noImageFound}
-									alt={movie.name || movie.title || movie.original_title}
-								/>
-							</li>
+							<Link to={`/movie/${movie.id}`}>
+								<li
+									className="similar-movies--item"
+									onClick={() => {
+										onClick(movie);
+										scrollToTop();
+									}}
+									key={i}
+								>
+									<img
+										loading="lazy"
+										src={movie.poster_path ? `${BASE_IMG_URL}${movie.poster_path}` : noImageFound}
+										alt={movie.name || movie.title || movie.original_title}
+									/>
+								</li>
+							</Link>
 						);
 					})}
 				</ul>
