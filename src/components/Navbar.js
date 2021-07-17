fb313b6895e54cbde881768c4b91317f8229d543
shamar-morrison <shamar.webdev@gmail.com>
2021-07-17 13:07:06 -0500

    added new google adsense tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,7 +49,7 @@ const Navbar = () => {
 							<img src={logo} alt="Logo" />
 						</Link>
 					</div>
-					{/* <ul className="navbar-links" onClick={() => window.scrollTo(0, 0)}>
+					<ul className="navbar-links" onClick={() => window.scrollTo(0, 0)}>
 						<li className="nav-link">
 							Movies
 							<ul className="nav-link--dropdown">
@@ -68,7 +68,7 @@ const Navbar = () => {
 							</ul>
 						</li>
 						<li className="nav-link">People</li>
-					</ul> */}
+					</ul>
 					<SearchBar />
 				</nav>
 			</div>
