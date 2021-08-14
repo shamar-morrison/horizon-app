93faab1bfacb913b1905e614424a454a92b52f38
shamar-morrison <shamar.webdev@gmail.com>
2021-08-13 23:56:15 -0500

    add watch btn and trailer btn

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -70,7 +70,7 @@ const Navbar = () => {
 								</Link>
 							</ul>
 						</li>
-						<li className="nav-link">4K</li>
+						{/* <li className="nav-link">4K</li> */}
 					</ul>
 					<SearchBar />
 				</nav>
