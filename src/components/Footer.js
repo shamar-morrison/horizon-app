e027ed6026075f43882adc4b89b4de51fd6b80b2
Shamar Morrison <shamar.webdev@gmail.com>
2024-05-17 04:57:14 -0500

    chore: Update footer component with empty about attribute

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import logo from '../img/logo.png';
 
 const Footer = () => {
 	return (
-		<footer className="footer" id="footer">
+		<footer className="footer" id="footer" about=''>
 			<div className="logo-footer">
 				<img src={logo} alt="Logo" />
 			</div>
